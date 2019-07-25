6551724a078c14a88ef9964856fadd552a37ddfd
Louis Bertrand <adressepro111@pylott.yt>
2019-07-25 19:51:40 +0200

    custom class import example

---
+++ app/bis.php
@@ -0,0 +1,9 @@
+<?php
+
+    class bis
+    {
+        public function __construct() {
+            print("bis class\r\n");
+            return true;
+        }
+    }
--- app/main.php
+++ app/main.php
@@ -5,12 +5,17 @@
     import("base");
     import("stdio");
 
+    // in order to import a custom class
+    // call require_once $file function
+    require_once "bis.php";
+
     class main extends KEYPHPAPPLICATION
     {
         
         public function loop()
         {
             stdio::cout("Hello World!");
+            new bis;
             return false;
         }
 
